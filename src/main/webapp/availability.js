f04a5d32cafcc510852c5c19afeacaee883fedfa
Meagan Konst <meagankonst@google.com>
2020-07-01 21:32:52 +0000

    Prints one time slot as UTC.

--- src/main/webapp/availability.js
+++ src/main/webapp/availability.js
@@ -50,7 +50,8 @@ function switchTile(tile) {
 
 function loadAvailabilityTable() {
   let date = new Date();
-  let timeZoneOffset = date.getTimezoneOffset();
+  let timeZoneOffset = (-1)*date.getTimezoneOffset();
+  console.log(timeZoneOffset);
   fetch('/availabilityTable.jsp?timeZoneOffset=' + timeZoneOffset).then(response => response.text()).then(table => {
     console.log(table);
     document.getElementById('table-container').innerHTML = table;
