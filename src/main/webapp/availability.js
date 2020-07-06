1fcd0579601281be3a172e8186d92960fcebaece
Meagan Konst <meagankonst@google.com>
2020-07-06 17:37:55 +0000

    Uses the forEach tag to generate one day of time slots. Updates TODO statements.

--- src/main/webapp/availability.js
+++ src/main/webapp/availability.js
@@ -51,9 +51,7 @@ function switchTile(tile) {
 function loadAvailabilityTable() {
   let date = new Date();
   let timeZoneOffset = (-1)*date.getTimezoneOffset();
-  console.log(timeZoneOffset);
   fetch('/availabilityTable.jsp?timeZoneOffset=' + timeZoneOffset).then(response => response.text()).then(table => {
-    console.log(table);
     document.getElementById('table-container').innerHTML = table;
   });
 }
