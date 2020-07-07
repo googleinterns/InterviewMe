d1f965e8f4f19384a6a27e1093f666a577d7b35a
Meagan Konst <meagankonst@google.com>
2020-07-07 15:13:47 +0000

    Adds space between if and (.

--- src/main/webapp/availability.js
+++ src/main/webapp/availability.js
@@ -19,7 +19,7 @@ function onAvailabilityLoad() {
 
 // Switches a tile from green to white and vice versa when clicked.
 function switchTile(tile) {
-  if(tile.classList.contains('table-success')) {
+  if (tile.classList.contains('table-success')) {
     tile.classList.remove('table-success');
   } else {
     tile.classList.add('table-success');
