4f0131c5253ee529e89aca38da1653c979c46e57
Meagan Konst <meagankonst@google.com>
2020-07-09 16:57:46 +0000

    New implementation with only one iterative loop.

--- src/main/java/com/google/sps/data/AvailabilityTimeSlotGenerator.java
+++ src/main/java/com/google/sps/data/AvailabilityTimeSlotGenerator.java
@@ -35,8 +35,7 @@ public class AvailabilityTimeSlotGenerator {
   private static final int EARLIEST_HOUR = 8;
   private static final int LATEST_HOUR = 19;
   // A list of hours and minutes representing permitted time slots.
-  private List<HourAndMinute> candidateHoursAndMinutes;
-  private static int numberOfSlotsPerDay;
+  private static final ImmutableList<HourAndMinute> ALL_HOURS_AND_MINUTES = allHoursAndMinutes();
 
   @AutoValue
   abstract static class HourAndMinute {
@@ -62,16 +61,15 @@ public class AvailabilityTimeSlotGenerator {
     }
   }
 
-  public void init() {
-    candidateHoursAndMinutes = new ArrayList<HourAndMinute>();
+  private static ImmutableList<HourAndMinute> allHoursAndMinutes() {
+    ImmutableList.Builder<HourAndMinute> candidateHoursAndMinutes = ImmutableList.builder();
     for (int i = EARLIEST_HOUR; i < LATEST_HOUR + 1; i++) {
       candidateHoursAndMinutes.add(HourAndMinute.create(i, 0));
       candidateHoursAndMinutes.add(HourAndMinute.create(i, 15));
       candidateHoursAndMinutes.add(HourAndMinute.create(i, 30));
       candidateHoursAndMinutes.add(HourAndMinute.create(i, 45));
     }
-
-    numberOfSlotsPerDay = candidateHoursAndMinutes.size();
+    return candidateHoursAndMinutes.build();
   }
 
   /**
@@ -97,12 +95,30 @@ public class AvailabilityTimeSlotGenerator {
     int month = day.getMonthValue();
     int dayOfMonth = day.getDayOfMonth();
 
-    String date = formatDate(dayOfWeek, month, dayOfMonth);
-    List<String> times = availableStartTimes();
-    int numberOfSlotsPerDay = times.size();
-    List<String> utcEncodings = generateUTCEncodings(year, month, dayOfMonth, zoneId);
-    List<Boolean> selectedStatuses = getSelectedStatuses(numberOfSlotsPerDay, utcEncodings);
-    return generateTimeSlots(numberOfSlotsPerDay, utcEncodings, times, date, selectedStatuses);
+    String formattedDate = formatDate(dayOfWeek, month, dayOfMonth);
+
+    ImmutableList.Builder<AvailabilityTimeSlot> timeSlots = ImmutableList.builder();
+    for (HourAndMinute hourAndMinute : ALL_HOURS_AND_MINUTES) {
+      int hour = hourAndMinute.hour();
+      int minute = hourAndMinute.minute();
+      LocalDateTime localTime = LocalDateTime.of(year, month, dayOfMonth, hour, minute);
+      ZonedDateTime utcTime =
+          ZonedDateTime.of(localTime, zoneId).withZoneSameInstant(ZoneOffset.UTC);
+      String formattedUTCTime = utcTime.format(DateTimeFormatter.ISO_INSTANT);
+
+      int standardHour = hour;
+      if (hour > 12) {
+        standardHour = hour - 12;
+      }
+      String formattedTime =
+          String.format("%d:%02d %s", standardHour, minute, hour < 12 ? "AM" : "PM");
+
+      timeSlots.add(
+          AvailabilityTimeSlot.create(
+              formattedUTCTime, formattedTime, formattedDate, getSelectedStatus(formattedUTCTime)));
+    }
+
+    return timeSlots.build();
   }
 
   // Uses an Instant and a timezoneOffsetMinutes int to create a ZonedDateTime instance
@@ -122,73 +138,11 @@ public class AvailabilityTimeSlotGenerator {
     return String.format("%s %d/%d", dayOfWeek, month, dayOfMonth);
   }
 
-  // This method returns a list of UTC Strings for a single day, creating one String per Time
-  // Slot.
-  private static List<String> generateUTCEncodings(
-      int year, int month, int dayOfMonth, ZoneId zoneId) {
-    List<String> utcEncodings = new ArrayList<String>();
-    int[] minutes = {0, 15, 30, 45};
-    for (int i = EARLIEST_HOUR; i <= LATEST_HOUR; i++) {
-      for (int j = 0; j < 4; j++) {
-        LocalDateTime localTimeSlot = LocalDateTime.of(year, month, dayOfMonth, i, minutes[j]);
-        ZonedDateTime utcTimeSlot =
-            ZonedDateTime.of(localTimeSlot, zoneId).withZoneSameInstant(ZoneOffset.UTC);
-        String formattedUTCTimeSlot = utcTimeSlot.format(DateTimeFormatter.ISO_INSTANT);
-        utcEncodings.add(formattedUTCTimeSlot);
-      }
-    }
-    return utcEncodings;
-  }
-
-  // TODO: Access the time slots from data store using the utcEncondings to tell if they
-  // are selected or not. Can get rid of numberOfSlotsPerDay upon new implementation.
+  // TODO: Access the time slot from data store using the utcEnconding to tell if it
+  // is selected or not.
   // This method will tell whether or not a time slot has already been selected. (See
   // TODO above).
-  private static List<Boolean> getSelectedStatuses(
-      int numberOfSlotsPerDay, List<String> utcEncodings) {
-    List<Boolean> selectedStatuses = new ArrayList<Boolean>();
-    for (int i = 0; i < numberOfSlotsPerDay; i++) {
-      selectedStatuses.add(false);
-    }
-    return selectedStatuses;
-  }
-
-  // Returns a list of readable time Strings such as "8:00 AM".
-  private static List<String> availableStartTimes() {
-    Integer[] validHours = new Integer[LATEST_HOUR - EARLIEST_HOUR + 1];
-    for (int i = EARLIEST_HOUR; i < LATEST_HOUR + 1; i++) {
-      validHours[i - EARLIEST_HOUR] = i;
-    }
-    ImmutableList<Integer> supportedHours = ImmutableList.copyOf(validHours);
-    ImmutableList<Integer> supportedMinutes = ImmutableList.of(0, 15, 30, 45);
-    ImmutableList.Builder<String> timesBuilder = ImmutableList.builder();
-    for (Integer hour : supportedHours) {
-      for (Integer minutes : supportedMinutes) {
-        int standardHour = hour;
-        if (hour > 12) {
-          standardHour = hour - 12;
-        }
-        timesBuilder.add(
-            String.format("%d:%02d %s", standardHour, minutes, hour < 12 ? "AM" : "PM"));
-      }
-    }
-    return timesBuilder.build();
-  }
-
-  // This method combines the lists of previously generated information into a list of constructed
-  // AvailabilityTimeSlot objects.
-  private static List<AvailabilityTimeSlot> generateTimeSlots(
-      int numberOfSlotsPerDay,
-      List<String> utcEncodings,
-      List<String> times,
-      String date,
-      List<Boolean> selectedStatuses) {
-    List<AvailabilityTimeSlot> availabilityTimeSlots = new ArrayList<AvailabilityTimeSlot>();
-    for (int i = 0; i < numberOfSlotsPerDay; i++) {
-      availabilityTimeSlots.add(
-          AvailabilityTimeSlot.create(
-              utcEncodings.get(i), times.get(i), date, selectedStatuses.get(i)));
-    }
-    return availabilityTimeSlots;
+  private static boolean getSelectedStatus(String utcEncoding) {
+    return false;
   }
 }
