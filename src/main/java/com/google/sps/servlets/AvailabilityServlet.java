bba6f17208d85a26122ab3657510fb7644329444
Meagan Konst <meagankonst@google.com>
2020-07-10 23:14:29 +0000

    Attempts new strategy for request to JSON.

--- src/main/java/com/google/sps/servlets/AvailabilityServlet.java
+++ src/main/java/com/google/sps/servlets/AvailabilityServlet.java
@@ -16,8 +16,13 @@ package com.google.sps.servlets;
 
 import com.google.gson.Gson;
 import com.google.sps.data.AvailabilityJSONConverter;
+import java.io.BufferedReader;
+// import processing.data.JSONObject;
+// import java.net.http;
 import java.io.IOException;
 import java.util.ArrayList;
+// import javax.json.JsonException;
+// import org.json.simple.JSONObject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +33,41 @@ public class AvailabilityServlet extends HttpServlet {
   @Override
   public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
     System.out.println("MADE IT TO AVAILABILITY SERVLET");
+
+    BufferedReader reader = request.getReader();
+    StringBuffer buffer = new StringBuffer();
+    String timestamps = null;
+
+    while ((timestamps = reader.readLine()) != null) buffer.append(timestamps);
+
+    String result = buffer.toString();
+
+    System.out.println("result: " + result);
+
     Gson gson = new Gson();
-    System.out.println("Before being converted to JSON: " + request.getParameter("body")); 
+
+    // String[] utcEncodings = gson.fromJson(result, String[].class);
+    // System.out.println("Here's what we got from buffered reader: " + utcEncodings);
+
+    /*
+    StringBuffer buffer = new StringBuffer();
+    String timestamps = null;
+    try {
+      BufferedReader reader = request.getReader();
+      while ((timestamps = reader.readLine()) != null) buffer.append(timestamps);
+    } catch (Exception e) {
+      /*report an error
+    }
+
+    try {
+      JSONObject jsonObject = HTTP.toJSONObject(buffer.toString());
+    } catch (JSONException e) {
+      // crash and burn
+      throw new IOException("Error parsing JSON request string");
+    }
+    */
+
+    System.out.println("Before being converted to JSON: " + request.getParameter("body"));
     // This is null. Need different method for converting request to JSON.
     String jsonRequest = gson.toJson(request.getParameter("body"));
     System.out.println("Here's the json from the request body: " + jsonRequest);
