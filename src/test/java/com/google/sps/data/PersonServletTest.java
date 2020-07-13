a2474557243d803f09139d6b8a024eae8803bfb4
Claire <clairexy@google.com>
2020-07-13 18:56:11 +0000

    works now but what to compare?

--- src/test/java/com/google/sps/data/PersonServletTest.java
+++ src/test/java/com/google/sps/data/PersonServletTest.java
@@ -14,6 +14,7 @@
 
 package com.google.sps.data;
 
+import com.google.gson.Gson;
 import com.google.sps.servlets.PersonServlet;
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,15 +24,29 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /** */
 @RunWith(JUnit4.class)
 public final class PersonServletTest {
+  LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalUserServiceTestConfig());
 
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
   //
   @Test
-  public void postTwo() throws IOException, UnsupportedEncodingException {
-
+  public void getOneOutOfTwo() throws IOException, UnsupportedEncodingException {
     MockHttpServletRequest postRequest = new MockHttpServletRequest();
     postRequest.addParameter("user-email", "a@gmail.com");
     postRequest.addParameter("first-name", "a");
@@ -40,20 +55,28 @@ public final class PersonServletTest {
     postRequest.addParameter("job", "");
     postRequest.addParameter("linkedin", "");
 
+    // Post person a.
     PersonServlet personServlet = new PersonServlet();
     personServlet.init(new FakePersonDao());
     personServlet.doPost(postRequest, new MockHttpServletResponse());
 
+    // Post person b.
     postRequest.setParameter("user-email", "b@gmail.com");
     postRequest.addParameter("first-name", "b");
     postRequest.addParameter("last-name", "b");
     personServlet.doPost(postRequest, new MockHttpServletResponse());
 
+    // b is logged in.
+    helper.setEnvIsLoggedIn(true).setEnvEmail("b@gmail.com").setEnvAuthDomain("auth");
     MockHttpServletRequest getRequest = new MockHttpServletRequest();
-    getRequest.addParameter("user-email", "b@gmail.com");
+    getRequest.addParameter("email", "b@gmail.com");
     MockHttpServletResponse getResponse = new MockHttpServletResponse();
     personServlet.doGet(getRequest, getResponse);
 
-    System.out.println(getResponse.getContentAsString());
+    String json = getResponse.getContentAsString();
+    System.out.println(json);
+    Gson g = new Gson();
+    Person p = g.fromJson(json, Person.class);
+    System.out.println(p.firstName());
   }
 }
