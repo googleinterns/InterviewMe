c0d18c18457ab59bd0fc6234982ca0baee9a6547
Claire <clairexy@google.com>
2020-07-28 13:49:18 +0000

    secrets integrated

--- src/main/java/com/google/sps/data/CalendarAccess.java
+++ src/main/java/com/google/sps/data/CalendarAccess.java
@@ -15,99 +15,57 @@
 package com.google.sps.data;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
 import com.google.api.services.calendar.model.Events;
-
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
+import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import com.google.api.client.auth.oauth2.StoredCredential;
-import java.io.BufferedReader;
-import java.util.stream.Collectors;
 
-import com.google.api.services.calendar.*;
-import com.google.api.services.calendar.model.*;
-
+// Handles all things Google Calendar (event adding primarily). 
 public class CalendarAccess {
-  private static final String APPLICATION_NAME = "Interview Me";
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private Calendar service;
+  private static final String CALENDAR_ID =
+      "info@jqed.dev"; // also the person we're trying to impersonate
 
-  /**
-   * Global instance of the scopes required by this quickstart. If modifying these scopes, delete
-   * your previously saved tokens/ folder.
-   */
-  private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+  // TODO: remember to write tests in the code that calls CalendarAccess() that handle what happens per each exception
+  public CalendarAccess() throws GeneralSecurityException, IOException, URISyntaxException, Exception {
+    System.out.println("CalendarAccess()");
+    String  key = new SecretFetcher("interview-me-step-2020").getSecretValue("SERVICE_ACCT_KEY");
+    GoogleCredential credential =
+        GoogleCredential.fromStream(new ByteArrayInputStream(key.getBytes()))
+            .createScoped(Collections.singletonList(CalendarScopes.CALENDAR))
+            .createDelegated(CALENDAR_ID);
+    System.out.println("getServiceAccountUser: " + credential.getServiceAccountUser());
 
-  // private static final String KEY_PATH =
-  //     "src/main/resources/interview-me-step-2020-6ae1489eb3f9.json";
-  // private static final String KEY_PATH_RUN = "interview-me-step-2020-6ae1489eb3f9.json";
-
-  public CalendarAccess() {}
-
-  public void getService() throws GeneralSecurityException, IOException {
-    try {
-      System.out.println("getService()");
-      FileInputStream fstream =
-          new FileInputStream(
-              new File(
-                  this.getClass()
-                      .getResource("/interview-me-step-2020-627ae7fba2f5.json")
-                      .toURI()));
-      FileInputStream fstream2 =
-          new FileInputStream(
-              new File(
-                  this.getClass()
-                      .getResource("/interview-me-step-2020-627ae7fba2f5.json")
-                      .toURI()));
-      String result =
-          new BufferedReader(new InputStreamReader(fstream2))
-              .lines()
-              .collect(Collectors.joining("\n"));
-      System.out.println(result);
-      GoogleCredential credential = GoogleCredential.fromStream(fstream).createScoped(SCOPES);
-      // .createDelegated("interviewme.business@gmail.com");
-      System.out.println("getServiceAccountUser: " + credential.getServiceAccountUser());
-
-      final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-      Calendar calendarService =
-          new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-              .setApplicationName(APPLICATION_NAME)
-              .build();
-      System.out.println("calendarService built");
-      listNextTen(calendarService);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      e.printStackTrace();
-    }
+    service =
+        new Calendar.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(),
+                credential)
+            .setApplicationName("Interview Me CalendarAccess")
+            .build();
+    System.out.println("calendarService built");
   }
 
-  public static void listNextTen(Calendar service) throws IOException {
+  public void listNextTen() throws IOException {
     System.out.println("listNextTen");
-    System.out.println(service.calendars().get("interviewme.business@gmail.com").execute());
-    // System.out.println(service.calendars().setMaxResults(10).execute());
+    System.out.println(service.calendars().get(CALENDAR_ID).execute());
+
     // List the next 10 events from the primary calendar.
     DateTime now = new DateTime(System.currentTimeMillis());
     Events events =
@@ -135,14 +93,8 @@ public class CalendarAccess {
     }
   }
 
-  public static void addEvent(Credential credential) throws IOException, GeneralSecurityException {
-    // Build a new authorized API client service.
-    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-    Calendar service =
-        new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-            .setApplicationName(APPLICATION_NAME)
-            .build();
-    System.out.println("addEvent after service built");
+  public void addEvent() throws IOException, GeneralSecurityException {
+    System.out.println("addEvent");
 
     Event event =
         new Event()
@@ -150,20 +102,20 @@ public class CalendarAccess {
             .setLocation("800 Howard St., San Francisco, CA 94103")
             .setDescription("A chance to hear more about Google's developer products.");
 
-    DateTime startDateTime = new DateTime("2020-07-23T09:00:00-07:00");
+    DateTime startDateTime = new DateTime("2020-07-31T09:00:00-07:00");
     EventDateTime start =
         new EventDateTime().setDateTime(startDateTime).setTimeZone("America/Toronto");
     event.setStart(start);
     System.out.printf("%s (%s)\n", event.getSummary(), start);
 
-    DateTime endDateTime = new DateTime("2020-07-23T17:00:00-07:00");
+    DateTime endDateTime = new DateTime("2020-07-31T17:00:00-07:00");
     EventDateTime end = new EventDateTime().setDateTime(endDateTime).setTimeZone("America/Toronto");
     event.setEnd(end);
 
     EventAttendee[] attendees =
         new EventAttendee[] {
-          new EventAttendee().setEmail("lpage@example.com"),
-          new EventAttendee().setEmail("sbrin@example.com"),
+          new EventAttendee().setEmail("the.claire.yang@gmail.com"),
+          new EventAttendee().setEmail("clairexy@umich.edu"),
         };
     event.setAttendees(Arrays.asList(attendees));
     System.out.printf("added attendees");
@@ -179,8 +131,7 @@ public class CalendarAccess {
 
     event.setReminders(reminders);
 
-    String calendarId = "clairexy@google.com";
-    // event = service.events().insert(calendarId, event).execute();
+    event = service.events().insert(CALENDAR_ID, event).execute();
     // System.out.printf("Event created: %s\n", event.getHtmlLink());
   }
 }
