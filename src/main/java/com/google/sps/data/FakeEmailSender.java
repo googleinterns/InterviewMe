377b907cae545ad762de83a0f35a8f67d5eeb44d
Grant <gcommodore@google.com>
2020-08-04 16:27:03 +0000

    fix

--- src/main/java/com/google/sps/data/FakeEmailSender.java
+++ src/main/java/com/google/sps/data/FakeEmailSender.java
@@ -36,7 +36,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URL;
 
-// Handles sending emails.
+// /** Mimics sending emails with Sendgrid. */
 @WebServlet("/email")
 public class FakeEmailSender implements EmailSender {
   // The email messages are being sent from.
@@ -45,7 +45,6 @@ public class FakeEmailSender implements EmailSender {
 
   public FakeEmailSender(Email sender) throws IOException {
     this.sender = sender;
-    // this.sg = "";
   }
 
   // Sends an email from the "from" Email to the "to" Email, with specified subject and content.
--- src/main/java/com/google/sps/data/SendgridEmailSender.java
+++ src/main/java/com/google/sps/data/SendgridEmailSender.java
@@ -50,7 +50,7 @@ public class SendgridEmailSender implements EmailSender {
             new SecretFetcher("interview-me-step-2020").getSecretValue("SENDGRID_API_KEY"));
   }
 
-  // Sends an email from the "from" Email to the "to" Email, with specified subject and content.
+  // Sends an email from the "sender" Email to the "recipient" Email, with specified subject and content.
   @Override
   public int sendEmail(Email recipient, String subject, Content content)
       throws IOException, Exception {
